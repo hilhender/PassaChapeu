f33b449309c60f8f7743d8bce8b58c652f943272
Andre Scherma Soleo <andrescherma@MacBook-Pro-de-Andre.local>
2015-03-31 14:15:27 -0300

    Storyboard + Controllers Mudei uns nomes, que tava osso. Ainda falta implementar as coisas nos controlares.

---
+++ PassaChapeu/PassaChapeu/BalancoViewController.h
@@ -0,0 +1,13 @@
+//
+//  BalancoViewController.h
+//  PassaChapeu
+//
+//  Created by Andre Scherma Soleo on 31/03/15.
+//  Copyright (c) 2015 Marcos Kobuchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BalancoViewController : UIViewController
+
+@end
---
+++ PassaChapeu/PassaChapeu/ExpensesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExpensesViewController.h
+//  PassaChapeu
+//
+//  Created by Andre Scherma Soleo on 31/03/15.
+//  Copyright (c) 2015 Marcos Kobuchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpensesViewController : UIViewController
+
+@end
