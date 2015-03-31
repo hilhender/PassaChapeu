5369dab53ece17216c8a624f119eeeb6eeb5b6a6
Andre Scherma Soleo <andrescherma@MacBook-Pro-de-Andre.local>
2015-03-31 15:02:19 -0300

    A tabela de Events já está pronta para ser alimentada. É preciso ainda integrar com o model. O botão de novo já dá o segue para uma nova view, que por hora está em branco. É necessario ainda implementar a passagem dos itens da lista para Gastos/Participantes.

--- PassaChapeu/PassaChapeu/EventsViewController.m
+++ PassaChapeu/PassaChapeu/EventsViewController.m
@@ -29,25 +29,34 @@
 
 #pragma mark - UITableViewDataSource required methods
 
-- (UITableViewCell *)tableView:(UITableView *)tableView
-         cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
+    static NSString *CellIdentifier =@"EventCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier
+                                                            forIndexPath:indexPath];
+    
+    cell.textLabel.text = @"porhoraalgumacoisa";
+    
+    return cell;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView
  numberOfRowsInSection:(NSInteger)section{
     
-    
+    return 5;
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
+/*
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
+ */
+
+
 
 @end
