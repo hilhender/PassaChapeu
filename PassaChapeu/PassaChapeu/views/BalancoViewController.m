d35099b1a9335e3560c3b169ebd112cf094f033e
Andre Scherma Soleo <andrescherma@MacBook-Pro-de-Andre.local>
2015-04-06 10:56:02 -0300

    Tentativa de mudar a cor em andamento

--- PassaChapeu/PassaChapeu/views/BalancoViewController.m
+++ PassaChapeu/PassaChapeu/views/BalancoViewController.m
@@ -22,47 +22,66 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    /* Exibe as informacoes na tela. */
-    NSMutableString *text = [[NSMutableString alloc] init];
-
-    [text appendString:@"Resumo Evento\n\n"];
-    [text appendFormat:@"  %-22s R$%.2f\n", "Valor gasto:", _controller.getTotalCost];
-    [text appendFormat:@"  %-22s R$%.2f\n", "Valor contribuido:", _controller.getContributedValue];
-    [text appendFormat:@"  %-22s R$%.2f\n", "Subtotal:", _controller.getTotalCost - _controller.getContributedValue];
-
+    NSMutableAttributedString *text = [[NSMutableAttributedString alloc] init];
+    NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] init];
     
-    [text appendString:@"\nDetalhes Pessoa\n"];
+    
+     /* Exibe as informacoes na tela. */
+    [text.mutableString appendString:@"Resumo Evento\n\n"];
+    [text.mutableString appendFormat:@"  %-22s R$%.2f\n", "Valor gasto:", _controller.getTotalCost];
+    
+    [text.mutableString appendFormat:@"  %-22s R$%.2f\n", "Valor contribuido:", _controller.getContributedValue];
+    [text.mutableString appendFormat:@"  %-22s R$%.2f\n", "Subtotal:", _controller.getTotalCost - _controller.getContributedValue];
+    
+    
+    //seta a cor da string de acordo com o valor e apenda em text
+    [attributedText.mutableString setString: [NSString stringWithFormat: @"R$%.2f\n", _controller.getTotalCost - _controller.getContributedValue]];
+    if(_controller.getTotalCost - _controller.getContributedValue < 0)
+        [attributedText setAttributes: [NSDictionary dictionaryWithObject: [UIColor redColor] forKey: NSForegroundColorAttributeName] range: NSMakeRange (0, [attributedText.mutableString length])];
+    else
+        [attributedText setAttributes: [NSDictionary dictionaryWithObject: [UIColor greenColor] forKey: NSForegroundColorAttributeName] range: NSMakeRange (0, [attributedText.mutableString length])];
+    
+    [text appendAttributedString: attributedText];
+
+
+    //tentativa de fazer com que o resto do texto nao pegue o atributo da ultima attributedstring apendada
+    //dá crash, nao sei porque, talvez pelo range
+    /*
+     [text setAttributes:[NSDictionary dictionaryWithObject: [UIColor blackColor] forKey:NSForegroundColorAttributeName] range: NSMakeRange([text.mutableString length], [text.mutableString length])];
+     */
+    [text.mutableString appendString:@"\nDetalhes Pessoa\n"];
     
     for (Sharer *sharer in _controller.getSharers) {
-        [text appendFormat:@"  %@\n", sharer.name];
+        [text.mutableString appendFormat:@"  %@\n", sharer.name];
         for (Expense *expense in sharer.expenses) {
-            [text appendFormat:@"    %@\n", expense.name];
-            [text appendFormat:@"      %-18s R$%.2f\n", "Preco:", expense.value];
-            [text appendFormat:@"      %-18s R$%.2f\n", "Preco por pessoa:", expense.value / expense.getNumberOfSharers];
+            [text.mutableString appendFormat:@"    %@\n", expense.name];
+            [text.mutableString appendFormat:@"      %-18s R$%.2f\n", "Preco:", expense.value];
+            [text.mutableString appendFormat:@"      %-18s R$%.2f\n", "Preco por pessoa:", expense.value / expense.getNumberOfSharers];
         }
-        [text appendFormat:@"    %-20s R$%.2f\n", "Valor contribuido:", sharer.contributedValue];
-        [text appendFormat:@"    %-20s R$%.2f\n", "Valor gasto:", sharer.evaluateBalance];
-        [text appendFormat:@"    %-20s R$%.2f\n", "Subtotal:", sharer.contributedValue - sharer.evaluateBalance];
+        [text.mutableString appendFormat:@"    %-20s R$%.2f\n", "Valor contribuido:", sharer.contributedValue];
+        [text.mutableString appendFormat:@"    %-20s R$%.2f\n", "Valor gasto:", sharer.evaluateBalance];
+        [text.mutableString appendFormat:@"    %-20s R$%.2f\n", "Subtotal:", sharer.contributedValue - sharer.evaluateBalance];
     }
     
     
-    [text appendString:@"\nDetalhes Gasto\n"];
+    [text.mutableString appendString:@"\nDetalhes Gasto\n"];
 
     for (Expense *expense in _controller.getExpenses) {
-        [text appendFormat:@"  %@\n", expense.name];
-        [text appendFormat:@"    %-20s R$%.2f\n", "Preco:", expense.value];
-        [text appendFormat:@"    %-20s %d\n", "Participantes:", (int)expense.getNumberOfSharers];
+        [text.mutableString appendFormat:@"  %@\n", expense.name];
+        [text.mutableString appendFormat:@"    %-20s R$%.2f\n", "Preco:", expense.value];
+        [text.mutableString appendFormat:@"    %-20s %d\n", "Participantes:", (int)expense.getNumberOfSharers];
         for (Sharer *sharer in expense.sharers) {
-            [text appendFormat:@"      %@\n", sharer.name];
+            [text.mutableString appendFormat:@"      %@\n", sharer.name];
         }
-        [text appendFormat:@"    %-20s R$%.2f\n", "Valor por pessoa:", expense.value / expense.getNumberOfSharers];
+        [text.mutableString appendFormat:@"    %-20s R$%.2f\n", "Valor por pessoa:", expense.value / expense.getNumberOfSharers];
     }
 
     /* Seta edicao, fonte da text field e exibe. */
+    
     [self.txtReceipt setEditable:NO];
     [self.txtReceipt setFont:[UIFont fontWithName:@"Courier" size:16]];
-    self.txtReceipt.text = text;
-
+    
+    self.txtReceipt.attributedText = text;
 }
 
 - (void)didReceiveMemoryWarning {
